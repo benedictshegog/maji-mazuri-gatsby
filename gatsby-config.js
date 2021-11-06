56ece1b329d372afcf70c1c8754745eeaacd5eb3
Benedict <benedict.shegog@gmail.com>
2021-11-06 16:33:47 +0000

    Cleanup after failed attempt to upgrade to Gatsby 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,17 +37,5 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "React go",
-        short_name: "Reactgo",
-        start_url: "/",
-        background_color: "#f7f0eb",
-        theme_color: "#a2466c",
-        display: "standalone",
-        icon: "src/images/icon.png",
-      },
-    },
   ],
 };
